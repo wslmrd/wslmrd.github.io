485aad5597c4a4a9c6e663d7f33aa06518fa665b
wslmrd <wslmrd@gmail.com>
2021-02-24 12:01:24 -0300

    teste

--- wdc.js
+++ wdc.js
@@ -1,27 +1,11 @@
 (function () {
     var myConnector = tableau.makeConnector();
     myConnector.getSchema = function (schemaCallback) {
-
-    // var cols = [
-    // { id : "date", alias:"Date", dataType : tableau.dataTypeEnum.string },
-    // { id : "trips", alias: "Trips Per Day",dataType : tableau.dataTypeEnum.string },
-    // { id : "farebox",alias: "Farebox Per Day", dataType : tableau.dataTypeEnum.string },
-    // { id : "uniquemed",alias: "Unique Medallions", dataType : tableau.dataTypeEnum.string },
-    // { id : "uniquedrivers", alias: "Unique Drivers", dataType : tableau.dataTypeEnum.string},
-    // { id : "medperday", alias: "Medallions Per Day", dataType : tableau.dataTypeEnum.string },
-    // { id : "avg1", alias: "Average Days Medallions On Road",  dataType : tableau.dataTypeEnum.string },
-    // { id : "avg2", alias: "Avg Hours Per Day Per Medallion",  dataType : tableau.dataTypeEnum.string},
-    // { id : "avg3", alias:"Avg Days Drivers on Road",  dataType : tableau.dataTypeEnum.string},
-    // { id : "avg4", alias:"Avg Hours Per Day Per Driver", dataType : tableau.dataTypeEnum.string},
-    // { id : "avg5", alias: "Avg Minutes Per Trip",  dataType : tableau.dataTypeEnum.string},
-    // { id : "cc", alias: "Percent of Trips Paid with Credit Card", dataType : tableau.dataTypeEnum.string}
-    // ];
-
-    var colsMetricas = [
-        { id : "tipo", alias:"Tipo", dataType : tableau.dataType.string },
-        { id : "intencao", alias:"Intencao", dataType : tableau.dataType.string },
-        { id : "usuario", alias:"Usuario", dataType : tableau.dataType.string },
-    ];
+        var colsMetricas = [
+            { id : "tipo", alias:"tipo", dataType : tableau.dataType.string },
+            { id : "intencao", alias:"intencao", dataType : tableau.dataType.string },
+            { id : "usuario", alias:"usuario", dataType : tableau.dataType.string },
+        ];
 
     var tableInfo = {
     id : "uber",
@@ -34,32 +18,31 @@
     
     myConnector.getData = function(table, doneCallback) {
         $.getJSON("https://southamerica-east1-prodap-x.cloudfunctions.net/intencao_metricas", function(resp) {
-        var feat = resp.metricas;
-        tableData = [];
-        // Iterate over the JSON object
-        for (var i = 0, len = feat.length; i < len; i++) {
-        tableData.push({
-        "tipo": feat[i]["Tipo"],
-        "intencao": feat[i]["Intencao"],
-        "usuario": feat[i]["Usuario"]
-        // "date": feat[i]["Month"]["Year"],
-        // "trips": feat[i]["Trips Per Day"],
-        // "farebox": feat[i] ["Farebox Per Day"],
-        // "uniquemed": feat[i] ["Unique Medallions"],
-        // "uniquedrivers": feat[i] ["Unique Drivers"],
-        // "medperday": feat[i] ["Medallions Per Day"],
-        // "avg1": feat[i] ["Avg Days Medallions on Road"],
-        // "avg2": feat[i] ["Avg Hours Per Day Per Medallion"] ,
-        // "avg3": feat[i] ["Avg Days Drivers on Road"],
-        // "avg4": feat[i] ["Avg Hours Per Day Per Driver"],
-        // "avg5": feat[i] ["Avg Minutes Per Trip"],
-        // "cc": feat[i] ["Percent of Trips Paid with Credit Card"]
+            var feat = resp.metricas,
+                tableData = [];
+            // Iterate over the JSON object
+            for (var i = 0, len = feat.length; i < len; i++) {
+                tableData.push({
+                "tipo": feat[i].tipo,
+                "intencao": feat[i].intencao,
+                "usuario": feat[i].usuario
+                // "date": feat[i]["Month"]["Year"],
+                // "trips": feat[i]["Trips Per Day"],
+                // "farebox": feat[i] ["Farebox Per Day"],
+                // "uniquemed": feat[i] ["Unique Medallions"],
+                // "uniquedrivers": feat[i] ["Unique Drivers"],
+                // "medperday": feat[i] ["Medallions Per Day"],
+                // "avg1": feat[i] ["Avg Days Medallions on Road"],
+                // "avg2": feat[i] ["Avg Hours Per Day Per Medallion"] ,
+                // "avg3": feat[i] ["Avg Days Drivers on Road"],
+                // "avg4": feat[i] ["Avg Hours Per Day Per Driver"],
+                // "avg5": feat[i] ["Avg Minutes Per Trip"],
+                // "cc": feat[i] ["Percent of Trips Paid with Credit Card"]
+                });
+            }
+            table.appendRows(tableData);
+            doneCallback();
         });
-    }
-    
-    table.appendRows(tableData);
-    doneCallback();
-    });
     };
     tableau.registerConnector(myConnector);
     $(document).ready(function () {
