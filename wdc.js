0ad0f2224b15a4d0d0f8743b0216501fad77500b
wslmrd <wslmrd@gmail.com>
2021-02-24 12:29:09 -0300

    teste 1

--- wdc.js
+++ wdc.js
@@ -3,19 +3,16 @@
 
     myConnector.getSchema = function (schemaCallback) {
         var cols = [{
-            id: "id",
+            id: "tipo",
             dataType: tableau.dataTypeEnum.string
         }, {
-            id: "mag",
-            alias: "magnitude",
-            dataType: tableau.dataTypeEnum.float
-        }, {
-            id: "title",
-            alias: "title",
+            id: "intencao",
+            alias: "intencao",
             dataType: tableau.dataTypeEnum.string
         }, {
-            id: "location",
-            dataType: tableau.dataTypeEnum.geometry
+            id: "usuario",
+            alias: "usuario",
+            dataType: tableau.dataTypeEnum.string
         }];
     
         var tableSchema = {
@@ -28,17 +25,16 @@
     };
 
     myConnector.getData = function(table, doneCallback) {
-        $.getJSON("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson", function(resp) {
-            var feat = resp.features,
+        $.getJSON("https://southamerica-east1-prodap-x.cloudfunctions.net/intencao_metricas", function(resp) {
+            var feat = resp.metricas,
                 tableData = [];
     
             // Iterate over the JSON object
             for (var i = 0, len = feat.length; i < len; i++) {
                 tableData.push({
-                    "id": feat[i].id,
-                    "mag": feat[i].properties.mag,
-                    "title": feat[i].properties.title,
-                    "location": feat[i].geometry
+                    "tipo": feat[i].tipo,
+                    "intencao": feat[i].intencao,
+                    "usuario": feat[i].usuario,
                 });
             }
     
