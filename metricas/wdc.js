7647cc971a4d845514a9043656710f93574d0ba1
wslmrd <wslmrd@gmail.com>
2021-03-08 17:06:27 -0300

    testes4

--- metricas/wdc.js
+++ metricas/wdc.js
@@ -155,6 +155,8 @@
             for (var i = 0, len = feat.length; i < len; i++) {
 				const dispositivo = feat[i].dispositivo || {}
                 const dialogo = feat[i].dialogo || {}
+                var fazendas = dialogo.fazendas;
+                for(var j = 0, len1 = fazendas.length; j < len1; j++) {
 			
 				tableData.push({
                     "tipo": feat[i].tipo,
@@ -189,11 +191,9 @@
                     "dataAlteracao_Dialogo": dialogo.dataAlteracao,
                     "descricao_Dialogo": dialogo.descricao,
                     "data_inicio_Dialogo": dialogo.inicio,
-                    "nomeFazenda_Dialogo": dialogo.fazendas.nomeFazenda
-
-
+                    "nomeFazenda_Dialogo": fazendas[j].nomeFazenda
                 });
-            }
+            }}
     
             table.appendRows(tableData);
             doneCallback();
